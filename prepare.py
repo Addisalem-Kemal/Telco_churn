dd61f90170d6f8087daeeac4746317327bb7907b
Addisalem-Kemal <102181167+Addisalem-Kemal@users.noreply.github.com>
2022-08-23 10:52:09 -0500

    Add files via upload

--- prepare.py
+++ prepare.py
@@ -1,57 +1,42 @@
-#standard ds libraries
-import pandas as pd
-import numpy as np
+def prep_telco_data(df):
 
-# import splitting functions
-from sklearn.model_selection import train_test_split
+    df[df.total_charges.str.match(
+        r'(^[0-9]+\.*[0-9]*$)') == False][['tenure', 'monthly_charges', 'total_charges']]
+    df.total_charges.str.replace(r'(^ $)', '0.00')
+    pd.to_numeric(df['total_charges'])
+    df['Female'] = df['gender'].apply(lambda x: 1 if x == 'Female' else 0)
+    cols_to_encode = ['partner', 'dependents', 'paperless_billing']
+    df[cols_to_encode] = df[cols_to_encode].replace({'No': 0, 'Yes': 1})
+    df['churn'] = df[['churn']].replace({'Yes': 1, 'No': 0})
+    df['phone_service'] = df[['phone_service']].replace({'Yes': 1, 'No': 0})
+    df['multiple_lines'] = df[['multiple_lines']].replace(
+        {'Yes': 1, 'No phone service': 0, 'No': 0})
+    df['online_security'] = df[['online_security']].replace(
+        {'Yes': 1, 'No internet service': 0, 'No': 0})
+    df['online_backup'] = df[['online_backup']].replace(
+        {'Yes': 1, 'No internet service': 0, 'No': 0})
+    df['device_protection'] = df[['device_protection']].replace(
+        {'Yes': 1, 'No internet service': 0, 'No': 0})
+    df['tech_support'] = df[['tech_support']].replace(
+        {'Yes': 1, 'No internet service': 0, 'No': 0})
+    df['streaming_tv'] = df[['streaming_tv']].replace(
+        {'Yes': 1, 'No internet service': 0, 'No': 0})
+    df['streaming_movies'] = df[['streaming_movies']].replace(
+        {'Yes': 1, 'No internet service': 0, 'No': 0})
+    train, validate, test = split_telco_data(df)
 
-
-def prep_iris(iris):
-    iris = iris.drop(columns=['species_id','measurement_id'])
-    iris = iris.rename(columns={'species_name':'species'})
-    dummy_iris = pd.get_dummies(iris.species, drop_first=True)
-    iris = pd.concat([iris, dummy_iris], axis=1)
-    return iris
-
-
-def prep_titanic(titanic):
-    titanic = titanic.drop(columns=['embarked','class', 'age','deck'])
-    dummy_df = pd.get_dummies(data=titanic[['sex','embark_town']], drop_first=True)
-    titanic = pd.concat([titanic, dummy_df], axis=1)
-    
-    return titanic
-
-
-def prep_telco(telco):
-    telco = telco.drop(columns=['internet_service_type_id', 'contract_type_id', 'payment_type_id'])
-
-    telco['gender_encoded'] = telco.gender.map({'Female': 1, 'Male': 0})
-    telco['partner_encoded'] = telco.partner.map({'Yes': 1, 'No': 0})
-    telco['dependents_encoded'] = telco.dependents.map({'Yes': 1, 'No': 0})
-    telco['phone_service_encoded'] = telco.phone_service.map({'Yes': 1, 'No': 0})
-    telco['paperless_billing_encoded'] = telco.paperless_billing.map({'Yes': 1, 'No': 0})
-    telco['churn_encoded'] = telco.churn.map({'Yes': 1, 'No': 0})
-    
-    dummy_df = pd.get_dummies(telco[['multiple_lines', \
-                              'online_security', \
-                              'online_backup', \
-                              'device_protection', \
-                              'tech_support', \
-                              'streaming_tv', \
-                              'streaming_movies', \
-                              'contract_type', \
-                              'internet_service_type', \
-                              'payment_type'
-                            ]],
-                              drop_first=True)
-    telco = pd.concat( [telco, dummy_df], axis=1 )
-    
-    return telco
+    return train, validate, test
 
 
-def my_train_test_split(df, target):
-    
-    train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
-    train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train[target])
-    
+def split_telco_data(df):
+    '''
+    This function performs split on telco data, stratify churn.
+    Returns train, validate, and test dfs.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2,
+                                            random_state=42,
+                                            stratify=df.churn)
+    train, validate = train_test_split(train_validate, test_size=.3,
+                                       random_state=42,
+                                       stratify=train_validate.churn)
     return train, validate, test
